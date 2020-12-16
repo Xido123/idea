fbe52febae813c53598f86063edd0e70accf94b7
Xido123 <1661480468@qq.com>
2020-12-17 00:47:17 +0800

    1217

---
+++ first_flink/src/main/scala/Solution.scala
@@ -0,0 +1,10 @@
+/**
+ *
+ */
+object Solution {
+  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+    for(i <- 0 until nums.length-1; j<-i+1 until nums.length)
+      if(nums(i)+ nums(j) == target) return Array(i,j)
+    Array(0,0)
+  }
+}
